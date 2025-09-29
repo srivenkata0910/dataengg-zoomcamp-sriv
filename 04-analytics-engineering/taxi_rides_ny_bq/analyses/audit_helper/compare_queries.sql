274cf62381c91c1b504d7399faf8cf8c7e6ee7e7
Sri V <srivenkata0910@gmail.com>
2025-09-29 16:15:09 +0000

    Refactored by indenting and converting to lower case

--- 04-analytics-engineering/taxi_rides_ny_bq/analyses/audit_helper/compare_queries.sql
+++ 04-analytics-engineering/taxi_rides_ny_bq/analyses/audit_helper/compare_queries.sql
@@ -8,10 +8,10 @@
 #}
 
 -- use this set if you are comparing to another dbt model
-{% set old_etl_relation=ref('fct_orders_deprecated') %}
+{% set old_etl_relation=ref('customer_orders') %}
 
 -- this is your newly built dbt model 
-{% set dbt_relation=ref('fct_orders') %}
+{% set dbt_relation=ref('fct_customer_orders') %}
 
 {{ audit_helper.compare_relations(
     a_relation=old_etl_relation,
